d11382becab381b165f4625220f9ac131734feff
Luis Alvarez Sanchez <asluisinfo@gmail.com>
2021-11-13 18:31:19 -0800

    REMOVING ALL UI

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,8 @@
-import Controller.Controller;
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
 
-public class Main extends Application {
 
-    @Override
-    public void start(Stage primaryStage){
-
-        primaryStage.setTitle("LiLAC");
-        primaryStage.setHeight(1000);
-        primaryStage.setWidth(1000);
-        Controller ctrl = new Controller(primaryStage);
-        ctrl.start();
-    }
-
+public class Main{
     public static void main(String[] args){
-        launch(args);
+
     }
+
 }
